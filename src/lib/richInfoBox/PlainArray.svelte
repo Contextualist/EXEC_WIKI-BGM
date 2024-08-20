f04877be8161450cf7aa5e93ddc20fc7452fd749
Contextualist <harzjc@gmail.com>
2024-08-19 19:56:14 -0700

    tweak: RichInfoBox/PlainArray: array append -> array init

--- src/lib/richInfoBox/PlainArray.svelte
+++ src/lib/richInfoBox/PlainArray.svelte
@@ -44,17 +44,22 @@
 	<Cell bind:value={value[0]} {action} class="rich-infobox-key" />
 	<div class="rich-infobox-value" bind:this={currContainer}>
 		<div class="text-xs line-height-5 italic">
-			({value[1].length ? `列表，${value[1].length} 项` : '空列表'})
-			<button
-				title="追加列表项"
-				class="border-none bg-transparent align-bottom my--0.5 text-bgm-teal/50 cursor-pointer"
-				onclick={() => {
-					value[1].push(['', '']);
-					action.update();
-				}}
-			>
-				{@render appendSVG()}
-			</button>
+			{#if value[1].length}
+				(列表，{value[1].length} 项)
+			{:else}
+				(空列表)
+				<button
+					title="开始编辑列表"
+					class="border-none bg-transparent align-bottom my--0.5 text-bgm-teal/50 cursor-pointer"
+					onclick={() => {
+						value[1].push(['', '']);
+						action.update();
+						setTimeout(() => focusIth(0), 0);
+					}}
+				>
+					{@render init1SVG()}
+				</button>
+			{/if}
 		</div>
 		{#each value[1] as val_i, i (val_i)}
 			<div animate:flip={{ duration: 200 }} class="rich-infobox-array relative">
@@ -84,7 +89,7 @@
 	{@render entryMod()}
 </div>
 
-{#snippet appendSVG()}
+{#snippet init1SVG()}
 	<svg
 		xmlns="http://www.w3.org/2000/svg"
 		width="16"
@@ -96,9 +101,8 @@
 		stroke-linecap="round"
 		stroke-linejoin="round"
 	>
-		<circle cx="12" cy="12" r="10"></circle>
-		<line x1="12" y1="8" x2="12" y2="16"></line>
-		<line x1="8" y1="12" x2="16" y2="12"></line>
+		<polygon points="14 2 18 6 7 17 3 17 3 13 14 2"></polygon>
+		<line x1="3" y1="22" x2="21" y2="22"></line>
 	</svg>
 {/snippet}
 
